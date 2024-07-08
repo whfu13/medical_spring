da6e73c52d2985b752fe034d865118c466de0465
김정수 <whfu13@naver.com>
2024-07-08 18:08:18 +0900

    2024/07/08 Spring

---
+++ p0708_01/src/main/java/com/java/www/Ex0708_01.java
@@ -0,0 +1,20 @@
+package com.java.www;
+
+public class Ex0708_01 {
+
+	public static void main(String[] args) {
+//		Pencil4B pencil4B = new Pencil4B();
+//		pencil4B.use();
+//		Pencil6B pencil6B = new Pencil6B();
+//		pencil6B.use();
+		
+		Pencil pencil = new Pencil4B();
+		pencil.use();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ p0708_01/src/main/java/com/java/www/Pencil.java
@@ -0,0 +1,6 @@
+package com.java.www;
+
+public interface Pencil {
+	
+	public void use();
+}
