34bed494e9df36f342c10d0e5c5ba2a86ac510d9
김정수 <whfu13@naver.com>
2024-07-08 18:09:51 +0900

    2024/07/08 Spring

---
+++ p0708_03/src/main/java/com/java/controller/FrontController.java
@@ -0,0 +1,73 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+
+// get,post 방식 / put,delete 방식
+public class FrontController {
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+	// get, post 방식 둘다 사용
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	// get 방식
+	@GetMapping("/board/board")
+	public String board() {
+		return "board/board";
+	}
+	
+	// post 방식
+	@PostMapping("/doBoard")
+	public String doBoard() {
+		return "doBoard";
+	}
+	
+	@RequestMapping("/board/form")
+	public String form() {
+		return "board/form";
+	}
+	
+	@RequestMapping("/board/doForm")
+	public String doForm(HttpServletRequest request, Model model) {
+		System.out.println("bno : "+request.getParameter("bno"));
+		System.out.println("btitle : "+request.getParameter("btitle"));
+		model.addAttribute("bno",request.getParameter("bno"));
+		model.addAttribute("btitle",request.getParameter("btitle"));
+		
+		return "board/doForm2";
+	}
+		
+		@RequestMapping("/board/form2")
+		public String form2() {
+			return "board/form";
+		}
+		
+		@RequestMapping("/board/doForm2")
+		public ModelAndView doForm2(HttpServletRequest request) {
+			System.out.println("bno : "+request.getParameter("bno"));
+			System.out.println("btitle : "+request.getParameter("btitle"));
+			
+			ModelAndView mv = new ModelAndView();
+			mv.addObject("bno",request.getParameter("bno"));
+			mv.addObject("btitle",request.getParameter("btitle"));
+			mv.setViewName("board/doForm2");
+			
+			return mv;
+	}
+
+}
